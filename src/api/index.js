7db0eba86cbb8c56ce2048d25499cabb14046908
sundevildi <sundevildi@gmail.com>
2023-03-11 13:01:15 +0100

    fix https and http

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,14 @@
 import axios from 'axios'
 import { getItem } from '/src/helpers/persistanceStorage'
 
+const httpProtocol = process.env.VUE_APP_HTTP_PROTOCOL || 'http';
+const httpsProtocol = process.env.VUE_APP_HTTPS_PROTOCOL || 'https';
+
 axios.defaults.baseURL = 'https://shop-api.polandgroups.pl/api'
+
+axios.defaults.http = process.env.NODE_ENV === 'production' ? false : httpProtocol === 'http'
+axios.defaultsюhttps = process.env.NODE_ENV === 'production' ? true : httpsProtocol === 'https'
+
 
 axios.interceptors.request.use(config => {
   config.headers['Content-Type'] = 'application/json'
