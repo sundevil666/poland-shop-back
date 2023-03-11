6c1bd09ad54567430746eb4da00ba106cb3fda83
sundevildi <sundevildi@gmail.com>
2023-03-11 15:52:02 +0100

    added .htaccess for correct work spa

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   publicPath: '/poland-shop-back',
+  chainWebpack: config => {
+    config
+      .plugin('copy')
+      .tap(args => {
+        args[0].push({
+          from: 'public/.htaccess',
+          to: ''
+        })
+        return args
+      })
+  }
 }
